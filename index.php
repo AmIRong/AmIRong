c7c9b7018d33c24471689af8e9f63e43f83b308f
AmIRong <2977535700@qq.com>
2017-02-10 11:45:26 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,32 @@
+$_ENV['curapp']
+$_GET
+$url
+$domain
+$jump
+$_ENV['domain']
+$_ENV['defaultapp']
+$_ENV['hostarr']
+$_ENV['domainroot']
+$_G['setting']['holddomain']
+$_GET['gid']
+$_GET['mod']
+$_GET['fid']
+$_GET['topicid']
+$_GET['catid']
+$_GET['fromapp']
+$_GET['id']
+$_ENV['prefixdomain']
+C::app()->init_setting = true;
+C::app()->init_user = false;
+C::app()->init_session = false;
+C::app()->init_cron = false;
+C::app()->init_misc = false;
+C::app()->init();
+$apphost
+$domain['idtype']
+$_G['setting']['rewritestatus']
+$delimiter
+
+global $_G;
+$holdmainarr
+$ishold
